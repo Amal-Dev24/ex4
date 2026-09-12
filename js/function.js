a17004b241406bf495890d4aa3c8bf9ac37e3ddc
Amal-Dev24 <amalemara625@gmail.com>
2026-09-12 19:32:05 +0300

    Rename functions to follow camelCase convention

--- js/function.js
+++ js/function.js
@@ -1,23 +1,24 @@
-function openpopup() {
+function openPopup() {
     popup.classList.add("active");
     setTimeout(function () {
         popup.classList.add("show");
     }, 100)
 }
-function closepopup() {
+function closePopup() {
     popup.classList.remove("show")
     setTimeout(function () {
         popup.classList.remove("active");
     }, 1000)
 }
-function updateimg(imgsrc) {
-    popupimg.setAttribute("src", imgsrc)
+function updateImg(imgsrc) {
+    popupImg.setAttribute("src", imgsrc)
 }
-function updateindicators() {
-    let newindicator = popuplist[currentimgindex],
-        oldindicator = popup.querySelector("ul li.active");
-    oldindicator.classList.remove("active");
-    newindicator.classList.add("active");
+function updateIndicators() {
+    let newIndicator = popupList[currentImgIndex];
+    let oldIndicator = popup.querySelector("ul li.active");
+
+    oldIndicator.classList.remove("active");
+    newIndicator.classList.add("active");
 
 }
 
